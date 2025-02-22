810bf70b82209b47f60793b673ffd15cc3cf43c2
RangerWhiskey <139371204+RangerWhiskey@users.noreply.github.com>
2025-02-22 17:39:21 +0000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games {  'gta5' }
+
+author 'RangerOne'
+description 'A basic resource to allow players to place emergency calls with one command'
+version '1.0.0'
+
+-- What to run
+client_scripts 'client/*.lua'
+server_script 'server/*.lua'
